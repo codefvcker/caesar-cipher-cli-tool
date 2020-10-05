9d9d44b5c35f979891236fb4f8f9979682b773b4
alehsiarheyeu <alehsiarheyeu@gmail.com>
2020-10-05 22:13:47 +0300

    completed

--- src/readableStream.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const getReadableStream = input => {
-  return input
-    ? fs.createReadStream(input, 'utf8')
-    : process.stdin.setEncoding('utf8')
-}
-
-module.exports = getReadableStream
